a8f519d3fa9180472adef8aa9dd542be944c878b
umanghing3080 <work.umanghingu@gmail.com>
2022-09-16 15:44:54 +0530

    removed <iframe> google map and continue with react-goole-maps-api and added marker and onClick event to map that returns location information.

--- src/pages/AboutusPage.js
+++ src/pages/AboutusPage.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
+import React, { useState } from "react";
+import { useJsApiLoader, GoogleMap, MarkerF } from "@react-google-maps/api";
 import Layout from "../Layout/Layout";
 
 const center = { lat: 21.215284608632743, lng: 72.85702453927374 };
 function AboutusPage() {
+  const [position, setPosition] = useState(center);
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
   });
@@ -14,16 +15,25 @@ function AboutusPage() {
       </Layout>
     );
   }
+
+  const locationHandler = (e) => {
+    console.log("Lat:", e.latLng.lat(), "Lng:", e.latLng.lng());
+   setPosition({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+    
+    //<Marker position={position}></Marker>;
+  };
+  console.log(position);
+
   return (
     <Layout>
       <GoogleMap
+        onClick={locationHandler}
         center={center}
         zoom={15}
         mapContainerStyle={{ width: "1000px", height: "800px", margin: "2%" }}
       >
-        <Marker position={center} />
+        <MarkerF position={position} />
       </GoogleMap>
-      <iframe title="umang" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7603.986982287915!2d72.85380355710099!3d21.219265136436288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xac10d91eadb0a3ab!2zMjHCsDEyJzU2LjciTiA3MsKwNTEnMjUuNCJF!5e0!3m2!1sen!2sus!4v1663320287488!5m2!1sen!2sus" width="1000" height="800"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </Layout>
   );
 }
